794d995b7200291c2b0366b17c9139c9e7411d7f
bilpeixoto <g4briel.soarespeixoto@gmail.com>
2021-04-12 11:17:10 -0300

    Projeto Finalizado

---
+++ src/components/organisms/Services.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front End"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nobis dolorum quia harum repellendus nisi ad quasi? Ullam sunt hic iure harum porro ad, laboriosam veritatis natus quaerat sint eaque.
+      Loren"
+    />
+    <Service
+      title="Back End"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nobis dolorum quia harum repellendus nisi ad quasi? Ullam sunt hic iure harum porro ad, laboriosam veritatis natus quaerat sint eaque.
+      Loren"
+    />
+    <Service
+      title="Manutenção"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nobis dolorum quia harum repellendus nisi ad quasi? Ullam sunt hic iure harum porro ad, laboriosam veritatis natus quaerat sint eaque.
+      Loren"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Service } from '@/components/molecules'
+
+export default defineComponent({
+  components: { Service }
+})
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-createApp(App).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'normalize.css'
+import '@/assets/css/main.css'
+createApp(App).mount('#app')
